d3e11d03e0aec678166857071d12eb3e2b85703b
macarong2653-oss <macarong.2653@gmail.com>
2026-02-24 07:31:05 +0700

    Vừa tạo các form

---
+++ Memory Card/From/CaiDat/MenuSetting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory_Card.From.CaiDat
+{
+    public partial class MenuSetting : Form
+    {
+        public MenuSetting()
+        {
+            InitializeComponent();
+        }
+    }
+}
